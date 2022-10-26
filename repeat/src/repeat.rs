7c37470f3e006e46850e17ddf4a9b749706ceaeb
dave <davemollen@gmail.com>
2022-10-26 19:21:33 +0200

    improve skew and feedback

--- repeat/src/repeat.rs
+++ repeat/src/repeat.rs
@@ -5,6 +5,7 @@ use std::f32;
 pub struct Repeat {
   delay: DelayLine,
   smooth_freq: Lowpass,
+  smooth_skew: Lowpass,
 }
 
 impl Repeat {
@@ -12,6 +13,7 @@ impl Repeat {
     Self {
       delay: DelayLine::new(sample_rate as usize * 10, sample_rate),
       smooth_freq: Lowpass::new(sample_rate),
+      smooth_skew: Lowpass::new(sample_rate),
     }
   }
 
@@ -23,21 +25,34 @@ impl Repeat {
     }
   }
 
-  fn get_delay_time(&self, index: f32, time_in_ms: f32, skew: f32) -> f32 {
+  fn get_delay_time(&self, index: f32, time: f32, skew: f32) -> f32 {
     if skew == 0. {
-      time_in_ms * index
+      time * index
     } else {
-      let softer_skew = f32::powf(skew, 2.) * skew.signum();
-      f32::powf(softer_skew + 1., index) * time_in_ms
+      let exponential_skew = f32::powf(2.0, skew);
+      if exponential_skew > 1.0 {
+        f32::powf(exponential_skew, index) * time - time
+      } else {
+        (1. - f32::powf(exponential_skew, index - 1.)) * time + time
+      }
+    }
+  }
+
+  fn reverse_indices(&self, index: f32, input: f32, repeats: u32) -> f32 {
+    if input.signum() == 1. {
+      index
+    } else {
+      repeats as f32 - index - 1.0
     }
   }
 
   fn repeat(&mut self, freq: f32, repeats: u32, feedback: f32, skew: f32) -> f32 {
     let time_in_ms = 1000. / freq;
     let mut out = 0f32;
-    for i in 1..repeats {
+    for i in 0..repeats {
       let index = i as f32;
-      let multiplication = self.simulate_feedback(index, feedback);
+      let feedback_index = self.reverse_indices(index, feedback, repeats);
+      let multiplication = self.simulate_feedback(feedback_index, feedback.abs());
       let time = self.get_delay_time(index, time_in_ms, skew);
       out += self.delay.read(time, "step") * multiplication
     }
@@ -46,9 +61,10 @@ impl Repeat {
 
   pub fn run(&mut self, input: f32, freq: f32, repeats: f32, feedback: f32, skew: f32) -> f32 {
     self.delay.write(input);
-    let frequency = self.smooth_freq.run(freq, 3.);
-    let repeated = self.repeat(frequency, repeats as u32, feedback, skew);
-    input + repeated
+    let smoothed_freq = self.smooth_freq.run(freq, 3.);
+    let smoothed_skew = self.smooth_skew.run(skew, 3.);
+    let repeated = self.repeat(smoothed_freq, repeats as u32, feedback, smoothed_skew);
+    repeated
   }
 }
 
@@ -59,14 +75,17 @@ mod tests {
   #[test]
   fn feedback() {
     let repeater = Repeat::new(44100.);
+    assert_eq!(repeater.simulate_feedback(0.0, 1.0), 1.0);
     assert_eq!(repeater.simulate_feedback(1.0, 1.0), 1.0);
     assert_eq!(repeater.simulate_feedback(2.0, 1.0), 1.0);
     assert_eq!(repeater.simulate_feedback(3.0, 1.0), 1.0);
 
+    assert_eq!(repeater.simulate_feedback(0.0, 2.0), 1.0);
     assert_eq!(repeater.simulate_feedback(1.0, 2.0), 2.0);
     assert_eq!(repeater.simulate_feedback(2.0, 2.0), 4.0);
     assert_eq!(repeater.simulate_feedback(3.0, 2.0), 8.0);
 
+    assert_eq!(repeater.simulate_feedback(0.0, 0.5), 1.0);
     assert_eq!(repeater.simulate_feedback(1.0, 0.5), 0.5);
     assert_eq!(repeater.simulate_feedback(2.0, 0.5), 0.25);
     assert_eq!(repeater.simulate_feedback(3.0, 0.5), 0.125);
@@ -75,16 +94,35 @@ mod tests {
   #[test]
   fn delay_time() {
     let repeater = Repeat::new(44100.);
+    assert_eq!(repeater.get_delay_time(0.0, 100.0, 0.0), 0.0);
     assert_eq!(repeater.get_delay_time(1.0, 100.0, 0.0), 100.0);
     assert_eq!(repeater.get_delay_time(2.0, 100.0, 0.0), 200.0);
     assert_eq!(repeater.get_delay_time(3.0, 100.0, 0.0), 300.0);
 
-    assert_eq!(repeater.get_delay_time(1.0, 100.0, 1.0), 200.0);
-    assert_eq!(repeater.get_delay_time(2.0, 100.0, 1.0), 400.0);
-    assert_eq!(repeater.get_delay_time(3.0, 100.0, 1.0), 800.0);
+    assert_eq!(repeater.get_delay_time(0.0, 100.0, 1.0), 0.0);
+    assert_eq!(repeater.get_delay_time(1.0, 100.0, 1.0), 100.0);
+    assert_eq!(repeater.get_delay_time(2.0, 100.0, 1.0), 300.0);
+    assert_eq!(repeater.get_delay_time(3.0, 100.0, 1.0), 700.0);
 
-    assert_eq!(repeater.get_delay_time(1.0, 100.0, -0.5), 75.0);
-    assert_eq!(repeater.get_delay_time(2.0, 100.0, -0.5), 56.25);
-    assert_eq!(repeater.get_delay_time(3.0, 100.0, -0.5), 42.1875);
+    assert_eq!(repeater.get_delay_time(0.0, 100.0, 0.0), 0.0);
+    assert_eq!(repeater.get_delay_time(1.0, 100.0, -1.0), 100.0);
+    assert_eq!(repeater.get_delay_time(2.0, 100.0, -1.0), 150.0);
+    assert_eq!(repeater.get_delay_time(3.0, 100.0, -1.0), 175.0);
+  }
+
+  #[test]
+  fn reversed_feedback() {
+    let repeater = Repeat::new(44100.);
+    let repeats = 4;
+    let expected_results = vec![0.125, 0.25, 0.5, 1.0];
+
+    for i in 0..repeats {
+      let feedback_index = repeater.reverse_indices(i as f32, -1.0, repeats);
+      let expected_result = expected_results[i as usize];
+      assert_eq!(
+        repeater.simulate_feedback(feedback_index, 0.5),
+        expected_result
+      );
+    }
   }
 }
--- vst/src/lib.rs
+++ vst/src/lib.rs
@@ -4,134 +4,134 @@ extern crate vst;
 use repeat::Repeat;
 use std::sync::Arc;
 use vst::{
-    api::TimeInfo,
-    buffer::AudioBuffer,
-    host::Host,
-    plugin::{HostCallback, Info, Plugin, PluginParameters},
-    util::AtomicFloat,
+  api::TimeInfo,
+  buffer::AudioBuffer,
+  host::Host,
+  plugin::{HostCallback, Info, Plugin, PluginParameters},
+  util::AtomicFloat,
 };
 
 struct DmRepeat {
-    params: Arc<RepeatParameters>,
-    repeat: Repeat,
+  params: Arc<RepeatParameters>,
+  repeat: Repeat,
 }
 
 struct RepeatParameters {
-    freq: AtomicFloat,
-    repeats: AtomicFloat,
-    feedback: AtomicFloat,
-    skew: AtomicFloat,
+  freq: AtomicFloat,
+  repeats: AtomicFloat,
+  feedback: AtomicFloat,
+  skew: AtomicFloat,
 }
 
 impl Default for RepeatParameters {
-    fn default() -> Self {
-        Self {
-            freq: AtomicFloat::new(4.0),
-            repeats: AtomicFloat::new(4.0),
-            feedback: AtomicFloat::new(1.0),
-            skew: AtomicFloat::new(1.0),
-        }
+  fn default() -> Self {
+    Self {
+      freq: AtomicFloat::new(4.0),
+      repeats: AtomicFloat::new(4.0),
+      feedback: AtomicFloat::new(1.0),
+      skew: AtomicFloat::new(0.0),
     }
+  }
 }
 
 impl Default for DmRepeat {
-    fn default() -> Self {
-        Self {
-            params: Arc::new(RepeatParameters::default()),
-            repeat: Repeat::new(44100.),
-        }
+  fn default() -> Self {
+    Self {
+      params: Arc::new(RepeatParameters::default()),
+      repeat: Repeat::new(44100.),
     }
+  }
 }
 
 impl Plugin for DmRepeat {
-    fn new(host: HostCallback) -> Self {
-        fn get_sample_rate(info: TimeInfo) -> f64 {
-            info.sample_rate
-        }
-        let sample_rate = host.get_time_info(0).map(get_sample_rate).unwrap();
-        Self {
-            params: Arc::new(RepeatParameters::default()),
-            repeat: Repeat::new(sample_rate),
-        }
+  fn new(host: HostCallback) -> Self {
+    fn get_sample_rate(info: TimeInfo) -> f64 {
+      info.sample_rate
     }
-
-    fn set_sample_rate(&mut self, sample_rate: f32) {
-        self.repeat = Repeat::new(f64::from(sample_rate));
+    let sample_rate = host.get_time_info(0).map(get_sample_rate).unwrap();
+    Self {
+      params: Arc::new(RepeatParameters::default()),
+      repeat: Repeat::new(sample_rate),
     }
+  }
 
-    fn get_info(&self) -> Info {
-        Info {
-            name: "dm-Repeat".to_string(),
-            inputs: 1,
-            outputs: 1,
-            parameters: 4,
-            unique_id: 1357,
-            f64_precision: true,
-            ..Default::default()
-        }
+  fn set_sample_rate(&mut self, sample_rate: f32) {
+    self.repeat = Repeat::new(f64::from(sample_rate));
+  }
+
+  fn get_info(&self) -> Info {
+    Info {
+      name: "dm-Repeat".to_string(),
+      inputs: 1,
+      outputs: 1,
+      parameters: 4,
+      unique_id: 1357,
+      f64_precision: true,
+      ..Default::default()
     }
+  }
 
-    fn process(&mut self, buffer: &mut AudioBuffer<f32>) {
-        let freq = self.params.freq.get();
-        let repeats = self.params.repeats.get();
-        let feedback = self.params.feedback.get();
-        let skew = self.params.skew.get();
+  fn process(&mut self, buffer: &mut AudioBuffer<f32>) {
+    let freq = self.params.freq.get();
+    let repeats = self.params.repeats.get();
+    let feedback = self.params.feedback.get();
+    let skew = self.params.skew.get();
 
-        for (input_buffer, output_buffer) in buffer.zip() {
-            for (input_sample, output_sample) in input_buffer.iter().zip(output_buffer) {
-                *output_sample = self
-                    .repeat
-                    .run(*input_sample, freq, repeats, feedback, skew);
-            }
-        }
+    for (input_buffer, output_buffer) in buffer.zip() {
+      for (input_sample, output_sample) in input_buffer.iter().zip(output_buffer) {
+        *output_sample = self
+          .repeat
+          .run(*input_sample, freq, repeats, feedback, skew);
+      }
     }
+  }
 
-    fn get_parameter_object(&mut self) -> Arc<dyn PluginParameters> {
-        Arc::clone(&self.params) as Arc<dyn PluginParameters>
-    }
+  fn get_parameter_object(&mut self) -> Arc<dyn PluginParameters> {
+    Arc::clone(&self.params) as Arc<dyn PluginParameters>
+  }
 }
 
 impl PluginParameters for RepeatParameters {
-    fn get_parameter(&self, index: i32) -> f32 {
-        match index {
-            0 => ((self.freq.get() - 0.1) / 49.9).powf(0.333333),
-            1 => ((self.repeats.get() - 1.0) / 31.0),
-            2 => self.feedback.get() / 2.,
-            3 => self.skew.get() / 2.,
-            _ => 0.0,
-        }
+  fn get_parameter(&self, index: i32) -> f32 {
+    match index {
+      0 => ((self.freq.get() - 0.1) / 49.9).powf(0.333333),
+      1 => ((self.repeats.get() - 1.0) / 31.0),
+      2 => self.feedback.get() / 2.5 - 1.25,
+      3 => self.skew.get() / 2. + 1.,
+      _ => 0.0,
     }
+  }
 
-    fn get_parameter_text(&self, index: i32) -> String {
-        match index {
-            0 => format!("{:.2} hz", self.freq.get()),
-            1 => format!("{:.2}", self.repeats.get()),
-            2 => format!("{:.2}%", self.feedback.get() * 100.0),
-            3 => format!("{:.2}%", self.skew.get() * 100.0),
-            _ => "".to_string(),
-        }
+  fn get_parameter_text(&self, index: i32) -> String {
+    match index {
+      0 => format!("{:.2} hz", self.freq.get()),
+      1 => format!("{:.2}", self.repeats.get()),
+      2 => format!("{:.2}%", self.feedback.get() * 100.0),
+      3 => format!("{:.2}%", self.skew.get() * 100.0),
+      _ => "".to_string(),
     }
+  }
 
-    fn get_parameter_name(&self, index: i32) -> String {
-        match index {
-            0 => "Frequency",
-            1 => "Repeats",
-            2 => "Feedback",
-            3 => "Skew",
-            _ => "",
-        }
-        .to_string()
+  fn get_parameter_name(&self, index: i32) -> String {
+    match index {
+      0 => "Frequency",
+      1 => "Repeats",
+      2 => "Feedback",
+      3 => "Skew",
+      _ => "",
     }
+    .to_string()
+  }
 
-    fn set_parameter(&self, index: i32, val: f32) {
-        match index {
-            0 => self.freq.set(val.powf(3.) * 49.9 + 0.1),
-            1 => self.repeats.set(val * 31.0 + 1.0),
-            2 => self.feedback.set(val * 2.),
-            3 => self.skew.set(val * 2.),
-            _ => (),
-        }
+  fn set_parameter(&self, index: i32, val: f32) {
+    match index {
+      0 => self.freq.set(val.powf(3.) * 49.9 + 0.1),
+      1 => self.repeats.set(val * 31.0 + 1.0),
+      2 => self.feedback.set(val * 2.5 - 1.25),
+      3 => self.skew.set(val * 2. - 1.),
+      _ => (),
     }
+  }
 }
 
 plugin_main!(DmRepeat);
