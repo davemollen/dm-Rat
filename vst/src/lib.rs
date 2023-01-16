7f4cd8e3cf807cc152f09b2c7f47663a2e8353cf
dave <davemollen@gmail.com>
2023-01-16 09:45:13 +0100

    Move plugin parameter handling to separate file

--- vst/src/lib.rs
+++ vst/src/lib.rs
@@ -1,35 +1,17 @@
 #[macro_use]
 extern crate vst;
-
+mod repeat_parameters;
 use repeat::Repeat;
+use repeat_parameters::RepeatParameters;
 use std::sync::Arc;
 use vst::{
   buffer::AudioBuffer,
   plugin::{Category, Info, Plugin, PluginParameters},
-  util::AtomicFloat,
 };
 
 struct DmRepeat {
   params: Arc<RepeatParameters>,
   repeat: Repeat,
-}
-
-struct RepeatParameters {
-  freq: AtomicFloat,
-  repeats: AtomicFloat,
-  feedback: AtomicFloat,
-  skew: AtomicFloat,
-}
-
-impl Default for RepeatParameters {
-  fn default() -> Self {
-    Self {
-      freq: AtomicFloat::new(4.0),
-      repeats: AtomicFloat::new(4.0),
-      feedback: AtomicFloat::new(1.0),
-      skew: AtomicFloat::new(0.0),
-    }
-  }
 }
 
 impl Default for DmRepeat {
@@ -78,49 +60,6 @@ impl Plugin for DmRepeat {
 
   fn get_parameter_object(&mut self) -> Arc<dyn PluginParameters> {
     Arc::clone(&self.params) as Arc<dyn PluginParameters>
-  }
-}
-
-impl PluginParameters for RepeatParameters {
-  fn get_parameter(&self, index: i32) -> f32 {
-    match index {
-      0 => ((self.freq.get() - 0.1) / 49.9).powf(0.333333),
-      1 => (self.repeats.get() - 1.0) / 31.0,
-      2 => self.feedback.get() / 2.5 - 1.25,
-      3 => self.skew.get() / 2. + 1.,
-      _ => 0.0,
-    }
-  }
-
-  fn get_parameter_text(&self, index: i32) -> String {
-    match index {
-      0 => format!("{:.2} hz", self.freq.get()),
-      1 => format!("{:.2}", self.repeats.get()),
-      2 => format!("{:.2}%", self.feedback.get() * 100.0),
-      3 => format!("{:.2}%", self.skew.get() * 100.0),
-      _ => "".to_string(),
-    }
-  }
-
-  fn get_parameter_name(&self, index: i32) -> String {
-    match index {
-      0 => "Frequency",
-      1 => "Repeats",
-      2 => "Feedback",
-      3 => "Skew",
-      _ => "",
-    }
-    .to_string()
-  }
-
-  fn set_parameter(&self, index: i32, val: f32) {
-    match index {
-      0 => self.freq.set(val.powf(3.) * 49.9 + 0.1),
-      1 => self.repeats.set(val * 31.0 + 1.0),
-      2 => self.feedback.set(val * 2.5 - 1.25),
-      3 => self.skew.set(val * 2. - 1.),
-      _ => (),
-    }
   }
 }
 
---
+++ vst/src/repeat_parameters.rs
@@ -0,0 +1,62 @@
+use vst::{plugin::PluginParameters, util::AtomicFloat};
+
+pub struct RepeatParameters {
+  pub freq: AtomicFloat,
+  pub repeats: AtomicFloat,
+  pub feedback: AtomicFloat,
+  pub skew: AtomicFloat,
+}
+
+impl Default for RepeatParameters {
+  fn default() -> Self {
+    Self {
+      freq: AtomicFloat::new(4.0),
+      repeats: AtomicFloat::new(4.0),
+      feedback: AtomicFloat::new(1.0),
+      skew: AtomicFloat::new(0.0),
+    }
+  }
+}
+
+impl PluginParameters for RepeatParameters {
+  fn get_parameter(&self, index: i32) -> f32 {
+    match index {
+      0 => ((self.freq.get() - 0.1) / 49.9).powf(0.333333),
+      1 => (self.repeats.get() - 1.0) / 31.0,
+      2 => self.feedback.get() / 2.5 - 1.25,
+      3 => self.skew.get() / 2. + 1.,
+      _ => 0.0,
+    }
+  }
+
+  fn get_parameter_text(&self, index: i32) -> String {
+    match index {
+      0 => format!("{:.2} hz", self.freq.get()),
+      1 => format!("{:.2}", self.repeats.get()),
+      2 => format!("{:.2}%", self.feedback.get() * 100.0),
+      3 => format!("{:.2}%", self.skew.get() * 100.0),
+      _ => "".to_string(),
+    }
+  }
+
+  fn get_parameter_name(&self, index: i32) -> String {
+    match index {
+      0 => "Frequency",
+      1 => "Repeats",
+      2 => "Feedback",
+      3 => "Skew",
+      _ => "",
+    }
+    .to_string()
+  }
+
+  fn set_parameter(&self, index: i32, val: f32) {
+    match index {
+      0 => self.freq.set(val.powf(3.) * 49.9 + 0.1),
+      1 => self.repeats.set(val * 31.0 + 1.0),
+      2 => self.feedback.set(val * 2.5 - 1.25),
+      3 => self.skew.set(val * 2. - 1.),
+      _ => (),
+    }
+  }
+}
