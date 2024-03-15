72780ee9facfe659f202c8c8437455c7e7bc51e8
Dave Mollen <dave.mollen@infi.nl>
2024-03-15 13:44:25 +0100

    Compensate clipping volume

--- rat/src/clipper.rs
+++ rat/src/clipper.rs
@@ -18,8 +18,8 @@ impl Clipper {
       let x2 = x * x;
       let x3 = x2 * x;
       let x5 = x3 * x2;
-      let a = x + (f32x8::splat(0.16489087) * x3) + (f32x8::splat(0.00985468) * x5);
-      a / (f32x8::splat(1.0) + (a * a)).sqrt()
-    }) * 0.558838
+      let a = x + f32x8::splat(0.16489087) * x3 + f32x8::splat(0.00985468) * x5;
+      a / (f32x8::splat(1.0) + a * a).sqrt()
+    }) * 0.3728
   }
 }
