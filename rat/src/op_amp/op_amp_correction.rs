9f01e1b85fc9ad21b85e15e10709dd73dfabc814
Dave Mollen <dave.mollen@infi.nl>
2024-05-23 19:17:57 +0200

    More efficient op-amp correction

--- rat/src/op_amp/op_amp_correction.rs
+++ rat/src/op_amp/op_amp_correction.rs
@@ -1,7 +1,8 @@
 use crate::shared::one_pole_filter::OnePoleFilter;
 
-const MAX_DB_AT_ONE_HZ: f32 = 120.9794;
-const DECREASE_PER_DECADE: f32 = 20.;
+const MAX_GAIN_AT_ONE_HZ: f32 = 1119360.558108; // max gain at 1Hz = 120.9794dB
+const MIN_DISTORTION_GAIN: f32 = 1.; // min distortion gain = 0dB
+const MAX_DISTORTION_GAIN: f32 = 2307.231003; // max distortion gain = 67.261822dB
 
 pub struct OpAmpCorrection {
   filter: OnePoleFilter,
@@ -14,22 +15,9 @@ impl OpAmpCorrection {
     }
   }
 
-  fn atodb(&self, amplitude: f32) -> f32 {
-    20. * amplitude.log10()
-  }
-
-  fn get_db_gain(&self, distortion: f32) -> f32 {
-    let r1 = distortion * 100000.;
-    let r2 = 560.;
-    let r3 = 47.;
-    let gain = 1. + r1 / (r2 * r3 / (r2 + r3));
-    self.atodb(gain)
-  }
-
   fn get_cutoff_frequency(&self, distortion: f32) -> f32 {
-    let db_gain = self.get_db_gain(distortion);
-    let decade_fraction = (MAX_DB_AT_ONE_HZ - db_gain) / DECREASE_PER_DECADE;
-    10_f32.powf(decade_fraction)
+    let gain = distortion * (MAX_DISTORTION_GAIN - MIN_DISTORTION_GAIN) + MIN_DISTORTION_GAIN;
+    MAX_GAIN_AT_ONE_HZ / gain
   }
 
   pub fn process(&mut self, input: f32, distortion: f32) -> f32 {
